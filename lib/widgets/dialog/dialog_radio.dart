be738d971dac1e25f92dcd9e5c1bf20131879cae
LeeDaeGyu <ttap0704@naver.com>
2023-01-10 09:57:31 +0900

    set radio buttons

--- lib/widgets/dialog/dialog_radio.dart
+++ lib/widgets/dialog/dialog_radio.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pet_app/classes/mungroad_colors.dart';
 import 'package:pet_app/styles.dart';
 
 class DialogRadio extends ConsumerStatefulWidget {
@@ -13,8 +14,6 @@ class DialogRadio extends ConsumerStatefulWidget {
 }
 
 class DialogRadioState extends ConsumerState<DialogRadio> {
-  String _selectedIdx = '';
-
   @override
   Widget build(BuildContext context) {
     final double width = MediaQuery.of(context).size.width;
@@ -39,18 +38,9 @@ class DialogRadioState extends ConsumerState<DialogRadio> {
             const RadioTitleBox(
               title: '위치',
             ),
-            // ...widget.values.map((entry) {
-            //   int idx = widget.values.indexOf(entry);
-            //   return Radio<String>(
-            //     value: entry,
-            //     groupValue: _selectedIdx,
-            //     onChanged: (String? value) {
-            //       setState(() {
-            //         _selectedIdx = value!;
-            //       });
-            //     },
-            //   );
-            // }).toList(),
+            RadioButtons(
+              values: widget.values,
+            ),
             SizedBox(
               width: double.infinity,
               child: ElevatedButton(
@@ -62,6 +52,92 @@ class DialogRadioState extends ConsumerState<DialogRadio> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class RadioButtons extends StatefulWidget {
+  const RadioButtons({
+    Key? key,
+    required this.values,
+  }) : super(key: key);
+
+  final List<String> values;
+
+  @override
+  State<RadioButtons> createState() => _RadioButtonsState();
+}
+
+class _RadioButtonsState extends State<RadioButtons> {
+  String _selectedValue = '';
+  List<Widget> _contents = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('hihi');
+    setContents();
+  }
+
+  void setContents() {
+    List<Widget> rowChildren = [];
+    List<Widget> tmpContents = [];
+    for (int i = 0, leng = widget.values.length; i < leng; i++) {
+      String currentValue = widget.values[i];
+
+      rowChildren.add(GestureDetector(
+        onTap: () {
+          print(currentValue);
+          setState(() {
+            _selectedValue = currentValue;
+          });
+        },
+        child: Row(
+          children: [
+            _selectedValue == currentValue
+                ? const Icon(
+                    Icons.radio_button_checked,
+                    color: MungroadColors.orange,
+                  )
+                : const Icon(
+                    Icons.radio_button_unchecked,
+                    color: Colors.black12,
+                  ),
+            Text(
+              currentValue,
+              style: TextStyle(
+                fontSize: multiplyFree(defaultSize, 1.3),
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                decoration: TextDecoration.none,
+              ),
+            ),
+          ],
+        ),
+      ));
+
+      tmpContents.add(Row(
+        children: [...rowChildren],
+      ));
+      if (i % 2 == 1) {
+        rowChildren = [];
+      }
+    }
+
+    print('111');
+    print(tmpContents);
+    setState(() {
+      _contents = [...tmpContents];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: _contents,
       ),
     );
   }
